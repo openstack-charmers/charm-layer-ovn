6a2dc912126111081b5279bbb899ebc03390b8da
Frode Nordahl <frode.nordahl@canonical.com>
2019-11-14 12:50:55 +0100

    Add helpers to communicate with OVS/OVN daemon control sockets

--- lib/charms/ovn.py
+++ lib/charms/ovn.py
@@ -12,7 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import json
+import os
 import subprocess
+
+
+OVS_RUNDIR = '/var/run/openvswitch'
 
 
 def _run(*args):
@@ -27,6 +31,84 @@ def _run(*args):
     return subprocess.run(
         args, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, check=True,
         universal_newlines=True)
+
+
+def ovs_appctl(target, *args):
+    """Run `ovs-appctl` for target with args and return output.
+
+    :param target: Name of daemon to contact.  Unless target begins with '/',
+                   `ovs-appctl` looks for a pidfile and will build the path to
+                   a /var/run/openvswitch/target.pid.ctl for you.
+    :type target: str
+    :param args: Command and arguments to pass to `ovs-appctl`
+    :type args: Tuple[str, ...]
+    :returns: Output from command
+    :rtype: str
+    :raises: subprocess.CalledProcessError
+    """
+    # NOTE(fnordahl): The ovsdb-server processes for the OVN databases use a
+    # non-standard naming scheme for their daemon control socket and we need
+    # to pass the full path to the socket.
+    if target in ('ovnnb_db', 'ovnsb_db',):
+        target = os.path.join(OVS_RUNDIR, target + '.ctl')
+    return _run('ovs-appctl', '-t', target, *args).stdout
+
+
+def cluster_status(target, schema=None):
+    """Retrieve status information from clustered OVSDB.
+
+    :param target: Usually one of 'ovsdb-server', 'ovnnb_db', 'ovnsb_db', can
+                   also be full path to control socket.
+    :type target: str
+    :param schema: Database schema name, deduced from target if not provided
+    :type schema: Optional[str]
+    :returns: Structured cluster status data
+    :rtype: Dict[str, Union[str, List[str], Tuple[str, str]]]
+    :raises: subprocess.CalledProcessError
+    """
+    schema_map = {
+        'ovnnb_db': 'OVN_Northbound',
+        'ovnsb_db': 'OVN_Southbound',
+    }
+    status = {}
+    k = ''
+    for line in ovs_appctl(
+            target,
+            'cluster/status',
+            schema or schema_map.get(target)).splitlines():
+        if k and line.startswith(' '):
+            status[k].append(line.lstrip())
+        elif ':' in line:
+            k, v = line.split(':', 1)
+            k = k.lower()
+            k = k.replace(' ', '_')
+            if v:
+                if k in ('cluster_id', 'server_id',):
+                    v = v.replace('(', '')
+                    v = v.replace(')', '')
+                    status[k] = tuple(v.split())
+                else:
+                    status[k] = v.lstrip()
+            else:
+                status[k] = []
+    return status
+
+
+def is_cluster_leader(target, schema=None):
+    """Retrieve status information from clustered OVSDB.
+
+    :param target: Usually one of 'ovsdb-server', 'ovnnb_db', 'ovnsb_db', can
+                   also be full path to control socket.
+    :type target: str
+    :param schema: Database schema name, deduced from target if not provided
+    :type schema: Optional[str]
+    :returns: Whether target is cluster leader
+    :rtype: bool
+    """
+    try:
+        return cluster_status(target, schema=schema).get('leader') == 'self'
+    except subprocess.CalledProcessError:
+        return False
 
 
 def add_br(bridge, external_id=None):
--- unit_tests/test_lib_charms_ovn.py
+++ unit_tests/test_lib_charms_ovn.py
@@ -5,9 +5,32 @@ import charms_openstack.test_utils as test_utils
 
 import charms.ovn as ovn
 
-VSCTL_BRIDGE_TBL = '''
+VSCTL_BRIDGE_TBL = """
 {"data":[[["uuid","1e21ba48-61ff-4b32-b35e-cb80411da351"],["set",[]],["set",[]],"0000a0369fdd3890","","<unknown>",["map",[["charm-ovn-chassis","managed"],["other","value"]]],["set",[]],["set",[]],["map",[]],["set",[]],false,["set",[]],"br-test",["set",[]],["map",[]],["set",[["uuid","617f9359-77e2-41be-8af6-4c44e7a6bcc3"],["uuid","da840476-8809-4107-8733-591f4696f056"]]],["set",[]],false,["map",[]],["set",[]],["map",[]],false],[["uuid","bb685b0f-a383-40a1-b7a5-b5c2066bfa42"],["set",[]],["set",[]],"00000e5b68bba140","","<unknown>",["map",[]],"secure",["set",[]],["map",[]],["set",[]],false,["set",[]],"br-int",["set",[]],["map",[["disable-in-band","true"]]],["set",[["uuid","07f4c231-9fd2-49b0-a558-5b69d657fdb0"],["uuid","8bbd2441-866f-4317-a284-09491702776c"],["uuid","d9e9c081-6482-4006-b7d6-239182b56c2e"]]],["set",[]],false,["map",[]],["set",[]],["map",[]],false]],"headings":["_uuid","auto_attach","controller","datapath_id","datapath_type","datapath_version","external_ids","fail_mode","flood_vlans","flow_tables","ipfix","mcast_snooping_enable","mirrors","name","netflow","other_config","ports","protocols","rstp_enable","rstp_status","sflow","status","stp_enable"]}
-'''
+"""
+
+CLUSTER_STATUS = """
+0ea6
+Name: OVN_Northbound
+Cluster ID: f6a3 (f6a36e77-97bf-4740-b46a-705cbe4fef45)
+Server ID: 0ea6 (0ea6e785-c2bb-4640-b7a2-85104c11a2c1)
+Address: ssl:10.219.3.174:6643
+Status: cluster member
+Role: follower
+Term: 3
+Leader: 22dd
+Vote: unknown
+
+Election timer: 1000
+Log: [2, 10]
+Entries not yet committed: 0
+Entries not yet applied: 0
+Connections: ->f6cf ->22dd <-22dd <-f6cf
+Servers:
+    0ea6 (0ea6 at ssl:10.219.3.174:6643) (self)
+    f6cf (f6cf at ssl:10.219.3.64:6643)
+    22dd (22dd at ssl:10.219.3.137:6643)
+"""
 
 
 class TestOVSDB(test_utils.PatchHelper):
@@ -19,6 +42,57 @@ class TestOVSDB(test_utils.PatchHelper):
         self.run.assert_called_once_with(
             ('aArg',), stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
             check=True, universal_newlines=True)
+
+    def test_ovs_appctl(self):
+        self.patch_object(ovn, '_run')
+        ovn.ovs_appctl('ovn-northd', 'is-paused')
+        self._run.assert_called_once_with('ovs-appctl', '-t', 'ovn-northd',
+                                          'is-paused')
+        self._run.reset_mock()
+        ovn.ovs_appctl('ovnnb_db', 'cluster/status')
+        self._run.assert_called_once_with('ovs-appctl', '-t',
+                                          '/var/run/openvswitch/ovnnb_db.ctl',
+                                          'cluster/status')
+        self._run.reset_mock()
+        ovn.ovs_appctl('ovnsb_db', 'cluster/status')
+        self._run.assert_called_once_with('ovs-appctl', '-t',
+                                          '/var/run/openvswitch/ovnsb_db.ctl',
+                                          'cluster/status')
+
+    def test_cluster_status(self):
+        self.patch_object(ovn, 'ovs_appctl')
+        self.ovs_appctl.return_value = CLUSTER_STATUS
+        expect = {
+            'name': 'OVN_Northbound',
+            'cluster_id': ('f6a3', 'f6a36e77-97bf-4740-b46a-705cbe4fef45'),
+            'server_id': ('0ea6', '0ea6e785-c2bb-4640-b7a2-85104c11a2c1'),
+            'address': 'ssl:10.219.3.174:6643',
+            'status': 'cluster member',
+            'role': 'follower',
+            'term': '3',
+            'leader': '22dd',
+            'vote': 'unknown',
+            'election_timer': '1000',
+            'log': '[2, 10]',
+            'entries_not_yet_committed': '0',
+            'entries_not_yet_applied': '0',
+            'connections': '->f6cf ->22dd <-22dd <-f6cf',
+            'servers': [
+                '0ea6 (0ea6 at ssl:10.219.3.174:6643) (self)',
+                'f6cf (f6cf at ssl:10.219.3.64:6643)',
+                '22dd (22dd at ssl:10.219.3.137:6643)',
+            ],
+        }
+        self.assertDictEqual(ovn.cluster_status('ovnnb_db'), expect)
+        self.ovs_appctl.assert_called_once_with('ovnnb_db', 'cluster/status',
+                                                'OVN_Northbound')
+
+    def test_is_cluster_leader(self):
+        self.patch_object(ovn, 'cluster_status')
+        self.cluster_status.return_value = {'leader': 'abcd'}
+        self.assertFalse(ovn.is_cluster_leader('ovnnb_db'))
+        self.cluster_status.return_value = {'leader': 'self'}
+        self.assertTrue(ovn.is_cluster_leader('ovnnb_db'))
 
     def test_add_br(self):
         self.patch_object(ovn, '_run')
