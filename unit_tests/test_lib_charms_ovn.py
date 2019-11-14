7c8cb6c6d8c5d788a85d559a23c53fa9e95db38d
Frode Nordahl <frode.nordahl@canonical.com>
2019-11-14 07:54:37 +0100

    Reorganize library placement in layer
    Use `lib/charms` instead of `lib/charm` to conform with how the
    majority of other team layers are organized.
    
    The main module contains helpers for more than just interacting
    with OVSDB, let's call it `ovn`.

--- unit_tests/test_lib_charms_ovn.py
+++ unit_tests/test_lib_charms_ovn.py
@@ -3,7 +3,7 @@ import subprocess
 
 import charms_openstack.test_utils as test_utils
 
-import charm.ovsdb as ovsdb
+import charms.ovn as ovn
 
 VSCTL_BRIDGE_TBL = '''
 {"data":[[["uuid","1e21ba48-61ff-4b32-b35e-cb80411da351"],["set",[]],["set",[]],"0000a0369fdd3890","","<unknown>",["map",[["charm-ovn-chassis","managed"],["other","value"]]],["set",[]],["set",[]],["map",[]],["set",[]],false,["set",[]],"br-test",["set",[]],["map",[]],["set",[["uuid","617f9359-77e2-41be-8af6-4c44e7a6bcc3"],["uuid","da840476-8809-4107-8733-591f4696f056"]]],["set",[]],false,["map",[]],["set",[]],["map",[]],false],[["uuid","bb685b0f-a383-40a1-b7a5-b5c2066bfa42"],["set",[]],["set",[]],"00000e5b68bba140","","<unknown>",["map",[]],"secure",["set",[]],["map",[]],["set",[]],false,["set",[]],"br-int",["set",[]],["map",[["disable-in-band","true"]]],["set",[["uuid","07f4c231-9fd2-49b0-a558-5b69d657fdb0"],["uuid","8bbd2441-866f-4317-a284-09491702776c"],["uuid","d9e9c081-6482-4006-b7d6-239182b56c2e"]]],["set",[]],false,["map",[]],["set",[]],["map",[]],false]],"headings":["_uuid","auto_attach","controller","datapath_id","datapath_type","datapath_version","external_ids","fail_mode","flood_vlans","flow_tables","ipfix","mcast_snooping_enable","mirrors","name","netflow","other_config","ports","protocols","rstp_enable","rstp_status","sflow","status","stp_enable"]}
@@ -13,41 +13,41 @@ VSCTL_BRIDGE_TBL = '''
 class TestOVSDB(test_utils.PatchHelper):
 
     def test__run(self):
-        self.patch_object(ovsdb.subprocess, 'run')
+        self.patch_object(ovn.subprocess, 'run')
         self.run.return_value = 'aReturn'
-        self.assertEquals(ovsdb._run('aArg'), 'aReturn')
+        self.assertEquals(ovn._run('aArg'), 'aReturn')
         self.run.assert_called_once_with(
             ('aArg',), stdout=subprocess.PIPE, stderr=subprocess.STDOUT,
             check=True, universal_newlines=True)
 
     def test_add_br(self):
-        self.patch_object(ovsdb, '_run')
-        ovsdb.add_br('br-x')
+        self.patch_object(ovn, '_run')
+        ovn.add_br('br-x')
         self._run.assert_called_once_with(
             'ovs-vsctl', 'add-br', 'br-x', '--', 'set', 'bridge', 'br-x',
             'protocols=OpenFlow13')
         self._run.reset_mock()
-        ovsdb.add_br('br-x', ('charm', 'managed'))
+        ovn.add_br('br-x', ('charm', 'managed'))
         self._run.assert_called_once_with(
             'ovs-vsctl', 'add-br', 'br-x', '--', 'set', 'bridge', 'br-x',
             'protocols=OpenFlow13', '--',
             'br-set-external-id', 'br-x', 'charm', 'managed')
 
     def test_del_br(self):
-        self.patch_object(ovsdb, '_run')
-        ovsdb.del_br('br-x')
+        self.patch_object(ovn, '_run')
+        ovn.del_br('br-x')
         self._run.assert_called_once_with(
             'ovs-vsctl', 'del-br', 'br-x')
 
     def test_add_port(self):
-        self.patch_object(ovsdb, '_run')
-        ovsdb.add_port('br-x', 'enp3s0f0')
+        self.patch_object(ovn, '_run')
+        ovn.add_port('br-x', 'enp3s0f0')
         self._run.assert_called_once_with(
             'ovs-vsctl', 'add-port', 'br-x', 'enp3s0f0')
 
     def test_list_ports(self):
-        self.patch_object(ovsdb, '_run')
-        ovsdb.list_ports('someBridge')
+        self.patch_object(ovn, '_run')
+        ovn.list_ports('someBridge')
         self._run.assert_called_once_with('ovs-vsctl', 'list-ports',
                                           'someBridge')
 
@@ -67,10 +67,10 @@ class TestSimpleOVSDB(Helper):
 
     def setUp(self):
         super().setUp()
-        self.target = ovsdb.SimpleOVSDB('atool', 'atable')
+        self.target = ovn.SimpleOVSDB('atool', 'atable')
 
     def test__find_tbl(self):
-        self.patch_object(ovsdb, '_run')
+        self.patch_object(ovn, '_run')
         cp = mock.MagicMock()
         cp.stdout = mock.PropertyMock().return_value = VSCTL_BRIDGE_TBL
         self._run.return_value = cp
@@ -115,7 +115,7 @@ class TestSimpleOVSDB(Helper):
             'atool', '-f', 'json', 'find', 'atable', 'name=br-test')
 
     def test_clear(self):
-        self.patch_object(ovsdb, '_run')
+        self.patch_object(ovn, '_run')
         self.target.clear('1e21ba48-61ff-4b32-b35e-cb80411da351',
                           'external_ids')
         self._run.assert_called_once_with(
@@ -123,7 +123,7 @@ class TestSimpleOVSDB(Helper):
             '1e21ba48-61ff-4b32-b35e-cb80411da351', 'external_ids')
 
     def test_remove(self):
-        self.patch_object(ovsdb, '_run')
+        self.patch_object(ovn, '_run')
         self.target.remove('1e21ba48-61ff-4b32-b35e-cb80411da351',
                            'external_ids', 'other')
         self._run.assert_called_once_with(
@@ -131,7 +131,7 @@ class TestSimpleOVSDB(Helper):
             '1e21ba48-61ff-4b32-b35e-cb80411da351', 'external_ids', 'other')
 
     def test_set(self):
-        self.patch_object(ovsdb, '_run')
+        self.patch_object(ovn, '_run')
         self.target.set('1e21ba48-61ff-4b32-b35e-cb80411da351',
                         'external_ids:other', 'value')
         self._run.assert_called_once_with(
