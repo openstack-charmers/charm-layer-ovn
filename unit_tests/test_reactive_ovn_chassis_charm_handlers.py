openstack-charmers/charm-layer-ovn

commit ba9ca99193324c69d6a4e82f64786aea26096140

Only initialize contexts for enabled features
To enable safe re-use of the configuration adapter in the OVN
Dedicated Chassis, guard initialization of optional contexts.

Related-Bug: #1906280

lib/charms/ovn_charm.py
@@ -48,11 +48,15 @@ class OVNConfigurationAdapter(
     def __init__(self, **kwargs):
         """Initialize contexts consumed from charm helpers."""
         super().__init__(**kwargs)
-        self._dpdk_device = self.OSContextObjectView(
-            os_context.DPDKDeviceContext(
-                bridges_key=self.charm_instance.bridges_key)())
-        self._sriov_device = os_context.SRIOVContext()
+        self._dpdk_device = None
+        self._sriov_device = None
         self._disable_mlockall = None
+        if ch_core.hookenv.config('enable-dpdk'):
+            self._dpdk_device = self.OSContextObjectView(
+                os_context.DPDKDeviceContext(
+                    bridges_key=self.charm_instance.bridges_key)())
+        if ch_core.hookenv.config('enable-sriov'):
+            self._sriov_device = os_context.SRIOVContext()
 
     @property
     def ovn_key(self):

unit_tests/test_lib_charms_ovn_charm.py
@@ -38,8 +38,12 @@ class TestOVNConfigurationAdapter(test_utils.PatchHelper):
         }
         self.patch('charmhelpers.contrib.openstack.context.SRIOVContext',
                    name='SRIOVContext')
+        m = mock.patch.object(ovn_charm.ch_core.hookenv, 'config')
+        m.start()
         self.target = ovn_charm.OVNConfigurationAdapter(
             charm_instance=self.charm_instance)
+        m.stop()
+        setattr(self, 'config', None)
 
     def test_ovn_key(self):
         self.assertEquals(self.target.ovn_key, '/etc/path/key_host')
@@ -99,12 +103,19 @@ class Helper(test_utils.PatchHelper):
             'driver': 'fakedriver',
         }
         self.patch_object(ovn_charm.ch_core.hookenv, 'config')
-        self.config.side_effect = lambda: config or {
-            'enable-hardware-offload': False,
-            'enable-sriov': False,
-            'enable-dpdk': False,
-            'bridge-interface-mappings': 'br-ex:eth0'
-        }
+
+        def _fake_config(x=None):
+            cfg = config or {
+                'enable-hardware-offload': False,
+                'enable-sriov': False,
+                'enable-dpdk': False,
+                'bridge-interface-mappings': 'br-ex:eth0'
+            }
+            if x:
+                return cfg.get(x)
+            return cfg
+
+        self.config.side_effect = _fake_config
         self.enable_openstack = mock.PropertyMock
         self.enable_openstack.return_value = False
         if release and release == 'train':

commit b4a1aa71802a6adaee0fd605c40f2af5d42f66e6

Fix missing init of SRIOV context for hw-offload
Commit ba9ca99193324c69d6a4e82f64786aea26096140 introduced a
regression that prohibits use of hw-offload due to not initializing
the SR-IOV context. Fix it.

lib/charms/ovn_charm.py
@@ -55,7 +55,8 @@ class OVNConfigurationAdapter(
             self._dpdk_device = self.OSContextObjectView(
                 os_context.DPDKDeviceContext(
                     bridges_key=self.charm_instance.bridges_key)())
-        if ch_core.hookenv.config('enable-sriov'):
+        if (ch_core.hookenv.config('enable-hardware-offload') or
+                ch_core.hookenv.config('enable-sriov')):
             self._sriov_device = os_context.SRIOVContext()
 
     @property

reactive/ovn_chassis_charm_handlers.py
@@ -63,6 +63,21 @@ def disable_openstack():
 
 
 @reactive.when_none('charm.paused', 'is-update-status-hook')
+@reactive.when(OVN_CHASSIS_ENABLE_HANDLERS_FLAG,
+               'config.changed.enable-hardware-offload')
+def ensure_networking_tools_installed():
+    """Ensure the networking tools are installed.
+
+    If the charm is used without OpenStack and hardware offload is enabled
+    post deploy the package may not be installed until we get here.
+    """
+    with charm.provide_charm_instance() as charm_instance:
+        charm_instance.install()
+        charm_instance.assess_status()
+        reactive.clear_flag('config.changed.enable-hardware-offload')
+
+
+@reactive.when_none('charm.paused', 'is-update-status-hook')
 @reactive.when(OVN_CHASSIS_ENABLE_HANDLERS_FLAG, 'nova-compute.connected')
 def enable_openstack():
     reactive.set_flag('charm.ovn-chassis.enable-openstack')

unit_tests/test_reactive_ovn_chassis_charm_handlers.py
@@ -47,6 +47,9 @@ class TestRegisteredHooks(test_utils.TestRegisteredHooks):
                 'enable_openstack': (
                     handlers.OVN_CHASSIS_ENABLE_HANDLERS_FLAG,
                     'nova-compute.connected',),
+                'ensure_networking_tools_installed': (
+                    handlers.OVN_CHASSIS_ENABLE_HANDLERS_FLAG,
+                    'config.changed.enable-hardware-offload',),
                 'configure_bridges': (
                     handlers.OVN_CHASSIS_ENABLE_HANDLERS_FLAG,
                     'config.rendered',),
@@ -64,6 +67,9 @@ class TestRegisteredHooks(test_utils.TestRegisteredHooks):
                     'is-update-status-hook',
                     'nova-compute.connected',),
                 'enable_openstack': ('charm.paused', 'is-update-status-hook'),
+                'ensure_networking_tools_installed': (
+                    'charm.paused',
+                    'is-update-status-hook'),
                 'configure_bridges': (
                     'charm.paused',
                     'is-update-status-hook'),